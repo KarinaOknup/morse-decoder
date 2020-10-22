e6dd11e41e47b73c75b60ce976557e8d33c69aa2
KarinaOknup <64832614+KarinaOknup@users.noreply.github.com>
2020-10-23 01:07:10 +0300

    add: solve task

--- src/index.js
+++ src/index.js
@@ -35,11 +35,47 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '':  ' ',
 };
 
 function decode(expr) {
-    // write your solution here
-}
+  let letter = [];
+    Array.from(expr)
+      for(let i = 0; i < expr.length; i += 10)
+  {
+      letter.push(expr.slice(i, i + 10));
+  }
+  let letter2=[];
+  for(let k=0;k<letter.length; k++){
+          let two = [];
+          Array.from(letter[k]);
+          for(let i = 0; i < letter[k].length; i += 2)
+            {
+            two.push(letter[k].slice(i, i + 2));
+            }
+          letter2.push(two);
+      }  
+  let code=[];
+  for(let k=0;k<letter2.length; k++){
+          let middlecode=[];
+          if(letter2[k]===[ '**', '**', '**', '**', '**' ]) middlecode.push(' ');
+          else{
+            for(let i = 0; i < letter2[k].length; i++)
+            {
+              if (letter2[k][i]==='00') middlecode.push('')
+              if (letter2[k][i]==='10') middlecode.push('.')
+              if (letter2[k][i]==='11') middlecode.push('-')
+            }
+      }  
+      code.push(middlecode);   
+  }
+  let str=[];
+  for(let k=0; k<code.length ;k++){
+      str.push(code[k].join(''));
+  }
+  str=str.map(letter=>MORSE_TABLE[letter]).join('');
+ return str;
+}  
 
 module.exports = {
     decode
